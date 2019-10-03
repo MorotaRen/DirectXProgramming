b902824fbf29ddc6abc883c4b1460ade656f3b35
MorotaRen <renwin25@gmail.com>
2019-10-03 10:20:58 +0900

    [Add]main.cppとWinMain関数の追加

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+///-------------------------------- <summary>
+/// MainŠÖ”‚Ì‘ã‚í‚è
+/// WINAPI‚ğ‚Â‚¯‚é‚±‚Æ
+/// </summary>-------------------------------
+int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int) {
+	return 0;
+}
